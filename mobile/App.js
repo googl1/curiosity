5cc5817ee51fee5cfcfec51de788c2479e70efb7
rick <rick.nitsche@studenti.unitn.it>
2018-03-17 16:18:11 -0700

    cleaned error

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import View from 'react-native';
 import CameraCapture from './components/CameraCapture.js'
-
+import firebaseConfig from './firebaseConfig.js' 
 import * as firebase from 'firebase';
 
 // Initialize Firebase
