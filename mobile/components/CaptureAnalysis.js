2fee3ddfbcd5840706735e45cbe1306967bb9a50
JeyzerMC <JeyzerMC@github.com>
2018-03-17 21:37:33 -0700

    Image split

--- mobile/components/CaptureAnalysis.js
+++ mobile/components/CaptureAnalysis.js
@@ -3,16 +3,14 @@ import {View, Text, Image} from 'react-native';
 
 class CaptureAnalysis extends React.Component {
     render() {
-        console.log(this.props.navigation.state.params.img)
         return (
             <View style={{ 
-                    flex: 1, 
-                    alignItems: 'center', 
-                    justifyContent: 'center' 
+                    flex: 2,
                 }}>
-                <Text>CaptureAnalysis Screen! </Text>
                 <Image source={{uri: this.props.navigation.state.params.img}}
-                        style={{width: 400, height: 400}}/>
+                        style={{flex: 1}}/>
+                <View style={{flex: 1, backgroundColor: 'green'}}>
+                </View>
             </View>
         );
     }
