27738e7de38660266664ebe0e5bc203299d6a37b
TaekHee Kwun <t.kwun@alumni.ubc.ca>
2018-03-18 00:45:50 -0700

    captureanalysis page non-functioning buttons

--- mobile/components/CaptureAnalysis.js
+++ mobile/components/CaptureAnalysis.js
@@ -10,10 +10,42 @@ class CaptureAnalysis extends React.Component {
                 <Image source={{uri: this.props.navigation.state.params.img}}
                         style={{flex: 3}}/>
                 <View style={{flex: 2, backgroundColor: 'mediumaquamarine'}}>
-                <Text style= {{ height:75, fontSize:25,color:'white',fontWeight:'bold'}}> Object Does Not Exist </Text>
-                <Text style = {{height:90,fontSize:20,color:'white',fontWeight:'bold'}}>Suggested Tags:</Text>
+                <Text style= {{ height:50, fontSize:25,color:'white',fontWeight:'bold'}}> Object Does Not Exist </Text>
+                <Text style = {{fontSize:20,color:'white',fontWeight:'bold'}}>Suggested Tags:</Text>
+                <View style={{  justifyContent:'center', flex:1}}>
+                <View style={{margin:20, flexDirection:'row', justifyContent:'space-between'}}>
+            <Button
+                title= "fungus"
+                color = "green"
+                
+            
+            />
+            <Button
+                title= "potato"
+                color = "green"
+            />
+            <Button
+                title= "vegetable"
+                color = "green"
+            />
+            </View>
+                </View>
+
             <Text style = {{fontSize:20,color:'white',fontWeight:'bold'}}>Not A Match?</Text>
         
+            <View style={{  justifyContent:'flex-end', flex:1}}>
+                <View style={{margin:20, flexDirection:'row', justifyContent:'space-between'}}>
+            <Button
+                title= "RE-TAKE"
+                color = "orange"
+            />
+            <Button
+                title= "ADD NEW"
+                color = "aqua"
+            />
+            </View>
+            </View>
+
                 </View>
                
             </View>
