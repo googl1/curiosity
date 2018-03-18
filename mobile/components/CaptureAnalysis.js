cb5052ee3de1fb2ce60a48f3fae61b6784c7a0d0
TaekHee Kwun <t.kwun@alumni.ubc.ca>
2018-03-17 23:36:54 -0700

    capture analysis minus buttons

--- mobile/components/CaptureAnalysis.js
+++ mobile/components/CaptureAnalysis.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {View, Text, Image} from 'react-native';
+import {View, Text, Image, StyleSheet, Button} from 'react-native';
 
 class CaptureAnalysis extends React.Component {
     render() {
         return (
             <View style={{ 
-                    flex: 2,
+                    flex: 1,
                 }}>
                 <Image source={{uri: this.props.navigation.state.params.img}}
-                        style={{flex: 1}}/>
-                <View style={{flex: 1, backgroundColor: 'green'}}>
+                        style={{flex: 3}}/>
+                <View style={{flex: 2, backgroundColor: 'mediumaquamarine'}}>
+                <Text style= {{ height:75, fontSize:25,color:'white',fontWeight:'bold'}}> Object Does Not Exist </Text>
+                <Text style = {{height:90,fontSize:20,color:'white',fontWeight:'bold'}}>Suggested Tags:</Text>
+            <Text style = {{fontSize:20,color:'white',fontWeight:'bold'}}>Not A Match?</Text>
+        
                 </View>
+               
             </View>
         );
     }
