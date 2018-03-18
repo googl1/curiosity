54ca673ad43951fb2860fc0838eb07d9852690d2
rick <rick.nitsche@studenti.unitn.it>
2018-03-18 01:43:12 -0700

    findEntriesWithTags

--- mobile/App.js
+++ mobile/App.js
@@ -2,26 +2,24 @@ import React from 'react';
 import CustomNavBar from './components/CustomNavBar'
 import View from 'react-native';
 import CameraCapture from './components/CameraCapture.js'
-import * as firebase from 'firebase';
 import Entry from './Entry.js';
-import FirebaseConfig from './FirebaseConfig.js';
-
+import Firebase from './Firebase.js';
 
 export default class App extends React.Component {
 componentDidMount() {
 
   // Initialize Firebase
-  let conf  = new FirebaseConfig();
-  firebase.initializeApp(conf.getConfig());
+  let fb = new Firebase();
 
   /*
   // dummy entries
   let e = new Entry("Dog", "George", "_image", ["animal", "friendly"]);
-  var db = firebase.database();
   let e1 = new Entry("Cat", "Bobby", "_image", ["animal", "evil"]);
   e.publish(db);
   e1.publish(db);
   */
+//  console.debug(fb.searchEntriesByTags(['orchid','cactus']));
+
 }
 
   render() {
